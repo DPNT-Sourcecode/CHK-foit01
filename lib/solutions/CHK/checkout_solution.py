65ce7ce836544fc9bb9c8efa3f18f2226c881aa2
sbx_user1051 <sbx_user1051@169.254.129.197>
2022-03-06 14:08:45 +0000

    Sun Mar 06 14:08:45 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,17 +3,13 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    basket = {e:skus.count(e) for e in set(skus)}
+    basket = {e: skus.count(e) for e in set(skus)}
     prices = {
-        'A': {
-            'price': 50,
-            'special_offer': [3, 130]
-        },
-        'B': {
-            'price': 30, 
-            'special_offer': [2, 45]
-            },
+        'A': {'price': 50, 'special_offer': [3, 130]},
+        'B': {'price': 30, 'special_offer': [2, 45]},
+        'C': {'price': 20},
+        'D': {'price': 15}
 
     }
-
+    return basket
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,4 +1,4 @@
 from lib.solutions.CHK import checkout_solution
 
 def test_checkout_solution():
-    assert checkout_solution.checkout('AAB') == 'hello'
+    assert checkout_solution.checkout('AAB') == {'A': 2, 'B':1}
