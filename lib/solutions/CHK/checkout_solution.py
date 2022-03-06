8baf5d6e7c9e3390410dec959df0794952b9cdc8
sbx_user1051 <sbx_user1051@169.254.15.237>
2022-03-06 15:44:26 +0000

    Sun Mar 06 15:44:26 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,13 +5,15 @@
 def checkout(skus):
     total_price = 0
     basket = {e: skus.count(e) for e in set(skus)}
+    print()
+    print('basket', basket)
     prices = {
         'A': {
             'price': 50,
             'special_offer': [
                 {
                     "type" : "cumulative",
-                    "offer": [3, 130]
+                    "offer": [5, 200]
                 },
                 {
                     "type" : "cumulative",
@@ -47,14 +49,20 @@ def checkout(skus):
         }
     }
     for item, amount in basket.items():
+
+        print(f'item: {item}, amount: {amount}')
+        special_offer = None
         try:
             if prices[item].get('special_offer', None):
                 for offer in prices[item].get('special_offer', None):
-                    special_offer = offer["offer"] if amount >= offer["offer"][0] else None
+                    if amount >= offer["offer"][0]:
+                        special_offer = offer["offer"]
         except KeyError:
             # Item not in the price table an offers
             return -1
+        print(f'special_offer: {special_offer}')
         has_special_offer = True if special_offer else False
+        print(f'has_special_offer: {has_special_offer}')
 
         if has_special_offer:           
             special_offer_amounts = (
@@ -68,4 +76,5 @@ def checkout(skus):
     return total_price
 
     
+
 
