5a7d4fa700c12e64f77f88a695ac29a1b9b2bc08
sbx_user1051 <sbx_user1051@169.254.90.113>
2022-03-06 16:35:16 +0000

    Sun Mar 06 16:35:16 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -2,7 +2,8 @@ from lib.solutions.CHK import checkout_solution
 
 def test_checkout_solution():
     assert checkout_solution.checkout('AAB') == 130
-    assert checkout_solution.checkout('BBBB') == 90
     assert checkout_solution.checkout('CCCCAAABB') == 255
     assert checkout_solution.checkout('XXXX') == -1
     assert checkout_solution.checkout('AAAAA') == 200
+    assert checkout_solution.checkout('BBBCCEE') == 165
+    assert checkout_solution.checkout('BAECDAACDE') == 280
