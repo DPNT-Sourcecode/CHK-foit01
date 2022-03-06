162e5795655a99e0d77601049802edfb4770f1c5
sbx_user1051 <sbx_user1051@169.254.129.197>
2022-03-06 14:23:45 +0000

    Sun Mar 06 14:23:45 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,8 +13,18 @@ def checkout(skus):
 
     }
     for item, amount in basket.items():
+        try:
+            special_offer = prices[item]['special_offer']
+            has_special_offer = amount >= special_offer[0]
+            
+            special_offer_amounts = (int(amount/special_offer[0]), amount%special_offer[0])
+            price = prices[item]['price'] if has_special_offer else 0
+        except KeyError:
+            # Noe special offers available
+            pass
         total_price += prices[item]['price'] * amount
     return total_price
 
     
+
 
