5ac757d0b79f6b76dec8d9e41e3a6bbcedc24e67
sbx_user1051 <sbx_user1051@169.254.129.197>
2022-03-06 14:29:45 +0000

    Sun Mar 06 14:29:45 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,6 +5,7 @@
 def checkout(skus):
     total_price = 0
     basket = {e: skus.count(e) for e in set(skus)}
+    print(basket)
     prices = {
         'A': {'price': 50, 'special_offer': [3, 130]},
         'B': {'price': 30, 'special_offer': [2, 45]},
@@ -15,10 +16,13 @@ def checkout(skus):
     for item, amount in basket.items():
         try:
             special_offer = prices[item]['special_offer']
+            print(special_offer)
             has_special_offer = amount >= special_offer[0]
+            print(has_special_offer)
             
             special_offer_amounts = (int(amount/special_offer[0]), amount%special_offer[0])
-            price = prices[item]['price'] if has_special_offer else 0
+            special_offer_price = (special_offer_amounts[0] * special_offer[1]) + (special_offer_amounts[1] * prices[item]['price'])
+            total_price += prices[item]['price'] if not has_special_offer else special_offer_price
         except KeyError:
             # Noe special offers available
             pass
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -2,4 +2,5 @@ from lib.solutions.CHK import checkout_solution
 
 def test_checkout_solution():
     assert checkout_solution.checkout('AAB') == 130
-    assert checkout_solution.checkout('BBBB') == 60
+    assert checkout_solution.checkout('BBBB') == 90
+    assert checkout_solution.checkout('CCCCAAABB') == 255
