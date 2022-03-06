664d56f98bc22c2001c0ffae4e37284ee55e1755
sbx_user1051 <sbx_user1051@169.254.129.197>
2022-03-06 14:54:55 +0000

    Sun Mar 06 14:54:55 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,9 +6,17 @@ def checkout(skus):
     total_price = 0
     basket = {e: skus.count(e) for e in set(skus)}
     prices = {
-        'A': {'price': 50, 'special_offer': [3, 130]},
-        'B': {'price': 30, 'special_offer': [2, 45]},
-        'C': {'price': 20},
+        'A': {
+            'price': 50,
+            'special_offer': [3, 130]
+        },
+        'B': {
+            'price': 30,
+            'special_offer': [2, 45]
+        },
+        'C': {
+            'price': 20
+        },
         'D': {'price': 15},
         'E': {'price': 40}, 'special_offer': []
 
@@ -33,6 +41,7 @@ def checkout(skus):
     return total_price
 
     
+
 
 
 
