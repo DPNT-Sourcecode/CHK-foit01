21352c6367aa8ed46be68ed33c3347b75ee24c3c
sbx_user1051 <sbx_user1051@169.254.129.197>
2022-03-06 14:05:45 +0000

    Sun Mar 06 14:05:45 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,5 +4,16 @@
 # skus = unicode string
 def checkout(skus):
     basket = {e:skus.count(e) for e in set(skus)}
-    return 'hello'
+    prices = {
+        'A': {
+            'price': 50,
+            'special_offer': [3, 130]
+        },
+        'B': {
+            'price': 30, 
+            'special_offer': [2, 45]
+            },
+
+    }
+
 
