fe3d67780e2dc1679263fc3d1e60e6511ecb58f5
sbx_user1051 <sbx_user1051@169.254.205.209>
2022-03-11 18:04:31 +0000

    Fri Mar 11 18:04:31 UTC 2022

--- lib/solutions/CHK/promotion.py
+++ lib/solutions/CHK/promotion.py
@@ -48,3 +48,15 @@ class Promotion:
         self.type = type
         self.discount_product = discount_product
         self.discount_qty = discount_qty
+
+    def __str__(self):
+        if self.type == 'free':
+            return f'F Prom: {self.discount_qty}{self.discount_product} free per {self.qualifying_qty}{self.qualifying_product}'
+        elif self.type == 'cumulative':
+            return f'C Prom: {self.discount_qty}£ free per {self.qualifying_qty}{self.qualifying_product}'
+    
+    def __repr__(self):
+        if self.type == 'free':
+            return f'F Prom: {self.discount_qty}{self.discount_product} free per {self.qualifying_qty}{self.qualifying_product}'
+        elif self.type == 'cumulative':
+            return f'C Prom: {self.discount_qty}£ free per {self.qualifying_qty}{self.qualifying_product}'
