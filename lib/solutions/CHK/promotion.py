cceb9dd5dfda4a92606c3e68437358b7b5237980
sbx_user1051 <sbx_user1051@169.254.205.209>
2022-03-11 17:58:31 +0000

    Fri Mar 11 17:58:31 UTC 2022

---
+++ lib/solutions/CHK/promotion.py
@@ -0,0 +1,21 @@
+PROMOTIONS = [
+    {
+        
+    }
+]
+
+class Promotion:
+    """
+    Our price table and offers: 
+    +------+-------+------------------------+
+    | Item | Price | Special offers         |
+    +------+-------+------------------------+
+    | A    | 50    | 3A for 130, 5A for 200 |
+    | B    | 30    | 2B for 45              |
+    | C    | 20    |                        |
+    | D    | 15    |                        |
+    | E    | 40    | 2E get one B free      |
+    +------+-------+------------------------+
+    """
+    def __init__(self):
+        pass
