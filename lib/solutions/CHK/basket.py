707756e54ee98d4005c7cc161ae594944fa5b068
sbx_user1051 <sbx_user1051@169.254.205.209>
2022-03-11 18:58:31 +0000

    Fri Mar 11 18:58:31 UTC 2022

--- lib/solutions/CHK/basket.py
+++ lib/solutions/CHK/basket.py
@@ -53,11 +53,14 @@ class Basket:
                             prod.promotion.qualifying_qty
                         )
                     num_promotions = prod.quantity // c_prom.qualifying_qty
+                    if num_promotions:
+                        prod.apply_promotion(c_prom)
                     total_discount += num_promotions * c_prom.discount_qty
         print(f'total discount {total_discount}')
         return total_discount
                     
                     
+
 
 
 
