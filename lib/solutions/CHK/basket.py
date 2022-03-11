fc5edd2cb6091c07cd01847dab55c6f1416664bc
sbx_user1051 <sbx_user1051@169.254.205.209>
2022-03-11 17:40:31 +0000

    Fri Mar 11 17:40:31 UTC 2022

---
+++ lib/solutions/CHK/basket.py
@@ -0,0 +1,25 @@
+
+
+
+class Basket():
+    """
+    Our price table and offers: 
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
++------+-------+------------------------+
+    """
+
+    def __init__(self, products, promotions):
+        self.products = products
+        self.promotions = promotions
+
+
+    @property
+    def get_total_price(self):
+        return 
--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -43,77 +43,12 @@ PRICES = {
             ]
         }
     }
-def sort_inside_value(elem):
-    return elem['offer'][0]
-
-def set_new_basket(skus, free_offers_list):
-    print(f'=========Set new basket function=========')
-    for free_offer_item in free_offers_list:
-        print(f'free offer item: {free_offer_item}')
-        for free_offer in sorted(
-            PRICES[free_offer_item]['special_offer'],
-            key=sort_inside_value,
-            reverse=True
-        ):
-            print(f'free offer: {free_offer}')
-            if skus.count(free_offer_item) >= free_offer['offer'][0]:
-                (quantity, free_prod) = free_offer['offer']
-                print(f'free_prod: {free_prod}')
-                skus = ''.join(sorted(skus)).replace(
-                    free_prod,
-                    '',
-                    int(skus.count(free_offer_item) / free_offer['offer'][0])
-                )
-                break
-    print(f'=========Set new basket function end========')
-    return skus
 
 def checkout(skus):
-    total_price = 0
-    free_offers_list = ['E']
-    print()
-    print('original skus', skus)
-    skus = set_new_basket(skus, free_offers_list)
-    print('new skus', skus)
-    basket = {e: skus.count(e) for e in set(skus)}
+    
 
-    print('basket', basket)
-
-    for item, amount in basket.items():
-
-        print(f'item: {item}, amount: {amount}')
-        special_offers = []
-        try:
-            if PRICES[item].get('special_offer', None):
-                for offer in sorted(
-                    PRICES[item].get('special_offer', None),
-                    key=sort_inside_value,
-                    reverse=True
-                ):
-                    if amount >= offer["offer"][0]:
-                        special_offers.append(offer)
-                        basket[item] -= offer["offer"][0]
-        except KeyError:
-            # Item not in the price table an offers
-            return -1
-        print(f'special_offer: {special_offers}')
-        has_special_offer = True if special_offers else False
-        print(f'has_special_offer: {has_special_offer}')
-
-        if has_special_offer:
-            for special_offer in special_offers:
-                special_offer_amounts = (
-                    int(amount/special_offer['offer'][0]),
-                    amount%special_offer['offer'][0]
-                ) 
-                print(f'special offer amount: {special_offer_amounts}')
-                special_offer_price = (special_offer_amounts[0] * special_offer['offer'][1]) \
-                    + (special_offer_amounts[1] * PRICES[item]['price'])
-        price = PRICES[item]['price'] * amount if not has_special_offer else special_offer_price
-        print('price', price)
-        total_price += price
-
-    return total_price
+    return 0
 
     
+
 
