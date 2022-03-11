112b3fc954027116a3d0ad1f32a51209d4742784
sbx_user1051 <sbx_user1051@169.254.205.209>
2022-03-11 17:46:31 +0000

    Fri Mar 11 17:46:31 UTC 2022

---
+++ lib/solutions/CHK/product.py
@@ -0,0 +1,23 @@
+PRODUCTS = {
+    'A': {'name': 'A', 'price': 50},
+    'B': {'name': 'B', 'price': 30},
+    'C': {'name': 'C', 'price': 20},
+    'D': {'name': 'D', 'price': 15},
+    'E': {'name': 'E', 'price': 40},
+}
+
+
+def create_products(skus: str) ->  list:
+
+
+
+
+class Product:
+
+    def __init__(self, name, quantity):
+        self.name = PRODUCTS[name]['name']
+        self.price = PRODUCTS[name]['price']
+        self.quantity = quantity
+
+    def __str__(self):
+        return f'{self.name} (£{self.price}'
