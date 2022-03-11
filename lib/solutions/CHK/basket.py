09872505d3248bf318b5b7963146af82c78fbc74
sbx_user1051 <sbx_user1051@169.254.205.209>
2022-03-11 17:43:31 +0000

    Fri Mar 11 17:43:31 UTC 2022

--- lib/solutions/CHK/basket.py
+++ lib/solutions/CHK/basket.py
@@ -1,18 +1,18 @@
 
 
 
-class Basket():
+class Basket:
     """
     Our price table and offers: 
-+------+-------+------------------------+
-| Item | Price | Special offers         |
-+------+-------+------------------------+
-| A    | 50    | 3A for 130, 5A for 200 |
-| B    | 30    | 2B for 45              |
-| C    | 20    |                        |
-| D    | 15    |                        |
-| E    | 40    | 2E get one B free      |
-+------+-------+------------------------+
+    +------+-------+------------------------+
+    | Item | Price | Special offers         |
+    +------+-------+------------------------+
+    | A    | 50    | 3A for 130, 5A for 200 |
+    | B    | 30    | 2B for 45              |
+    | C    | 20    |                        |
+    | D    | 15    |                        |
+    | E    | 40    | 2E get one B free      |
+    +------+-------+------------------------+
     """
 
     def __init__(self, products, promotions):
@@ -22,4 +22,4 @@ class Basket():
 
     @property
     def get_total_price(self):
-        return 
+        return sum([p for p in self.products])
